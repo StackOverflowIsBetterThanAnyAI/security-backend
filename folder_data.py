a6ab208358b8e2fc62b60ceccca9701f2b47b760
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-10-06 14:55:55 +0200

    folder data for testing / template for future implementation

---
+++ folder_data.py
@@ -0,0 +1,4 @@
+LOGGEDIN_USER = "user"
+IMAGE_FOLDER = "security/security_images"
+# IMAGE_FOLDER_LOCATION = f"/home/{LOGGEDIN_USER}/{IMAGE_FOLDER}"
+IMAGE_FOLDER_LOCATION = "C:/Users/micha/security/security_images"
