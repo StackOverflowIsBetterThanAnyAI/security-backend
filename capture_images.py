fac22540a57c3a41058c63677f0b08c81a0a758a
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-11-01 13:15:22 +0100

    use rpicam instead of picamzero

--- capture_images.py
+++ capture_images.py
@@ -1,19 +1,23 @@
 import os
 import re
+import subprocess
 
-from folder_data import IMAGE_FOLDER_LOCATION
-
-from picamzero import Camera
 from datetime import datetime
 from time import sleep
+from folder_data import IMAGE_FOLDER_LOCATION 
 
 REGEX = re.compile(r"^security_image_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.jpg$")
 
+RPICAM_CMD = [
+    "rpicam-jpeg",
+    "-t", "1",
+    "--width", "854",
+    "--height", "480",
+    "--nopreview"
+]
+
 
 def capture_images():
-    cam = Camera()
-    cam.still_size = (854, 480)
-
     if not os.path.exists(IMAGE_FOLDER_LOCATION):
         os.makedirs(IMAGE_FOLDER_LOCATION)
 
@@ -29,18 +33,17 @@ def capture_images():
             current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             filename = f"{IMAGE_FOLDER_LOCATION}/security_image_{current_time}.jpg"
 
-            cam.annotate(current_time)
-            cam.capture_image(filename)
-
+            command_to_run = RPICAM_CMD + ["--output", filename]
+            
+            subprocess.run(command_to_run, check=True) 
+            
             print(f"Image captured: {filename}.")
             print(f"{2016-len(image_files)} free image slots available.")
             sleep(300)
 
     except KeyboardInterrupt:
         print("Capturing images has been stopped.")
-
-    finally:
-        del cam
+    
 
 
 if __name__ == "__main__":
